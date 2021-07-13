1a9b3542c96c3a609c22182c3534250b39922413
jspearus <35698087+jspearus@users.noreply.github.com>
2021-07-13 14:55:06 -0500

    Update MIDITabletController.ino

--- MIDITabletController/MIDITabletController.ino
+++ MIDITabletController/MIDITabletController.ino
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 /* MIDI Tablet Controller 
  *  reads midi data and acts as a mouse to control
  *  a tablet
@@ -9,21 +10,34 @@
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
+// MIDI Channel and Note Setup ##############
 const int Channel = 2;
 const int Full_note = 75;
 const int Worship_note = 65;
 const int Velocity_thresh = 90;
+//###########################################
 
-
+//Variables
 int type, note, velocity, channel, d1, d2;
+
+//Function Declarations #############################
+void home();
+void moveCursor(int x, int y, int v);
+void leftClick();
+void printData();
+//#######################################
+
 
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
   Serial.begin(57600);
   delay(500);
   home();
-  //Serial.println("MIDI Input Test");
+  delay(2000);
+  Serial.println("MIDI Tablet Controller Ready...");
   } 
+
+// Main Program Loop########################################
 void loop() {
   if (MIDI.read()){
     byte type = MIDI.getType();
@@ -38,32 +52,35 @@ void loop() {
         break;
     }
   }
-  
-  if(channel == Channel && note == Full_note && velocity >= Velocity_thresh ){            //note = command for "full"
+  //note = command for "full"
+  if(channel == Channel && note == Full_note && velocity >= Velocity_thresh ){            
     //Serial.println("Full");
-    moveCursor(15, 45, 3);
+    moveCursor(15, 45, 3);  // move(x,y,v)
   }
-  
-   else if(channel == Channel && note == Worship_note && velocity >= Velocity_thresh){            //note = command for "Worship"
+  //note = command for "Worship"
+  else if(channel == Channel && note == Worship_note && velocity >= Velocity_thresh){            
     //Serial.println("Worship");
-    moveCursor(63, 47, 3);
-   }
+    moveCursor(63, 47, 3); // move(x,y,v)
+  }
 }
+ //END Main Loop ###########################################
+
+
+//Functions-------------------------------------------------
 
 void home(){
   int i;
     for (i=0; i<50; i++) {
-h     Mouse.move(-127, -127);
+     Mouse.move(-127, -127);
    }
 }
 
 void moveCursor(int x, int y, int v){
   home();
-  for (int i=0; i<x; i++){    // move Horizontal
+  for (int i=0; i<x; i++){    // move Horizontal - X AXIS
     Mouse.move(v, 0);
-  }
-  
-  for (int i=0; i<y; i++) {   // Move Verticl
+  } 
+  for (int i=0; i<y; i++) {   // Move Verticl - Y AXIS
     Mouse.move(0, v);
   }
   delay(25);
@@ -77,10 +94,11 @@ void leftClick(){
   Mouse.release();
   delay(250);
 }
+
 void printData(){
   if (velocity > 0) {
-          Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
-        } else {
-          Serial.println(String("Note Off: ch=") + channel + ", note=" + note);
-        }
+    Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+  } else {
+    Serial.println(String("Note Off: ch=") + channel + ", note=" + note);
+  }
 }
