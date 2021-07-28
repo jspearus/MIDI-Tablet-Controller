87c942e51231be75872a511840c4c9cf3f3e99b2
jspearus <35698087+jspearus@users.noreply.github.com>
2021-07-28 12:29:18 -0500

    Added I2C port
    Added I2c port for Expansion. Bluetooth module added

---
+++ MIDI_TC_Pro/src/main.cpp
@@ -0,0 +1,137 @@
+
+
+
+#include <Arduino.h>
+#include <MIDI.h>
+#include <Wire.h>
+
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+
+// MIDI Channel and Note Setup ##############
+const int Channel = 2;
+const int Full_note = 75;
+const int Worship_note = 65;
+const int Velocity_thresh = 90;
+//###########################################
+
+//Variables
+int type, note, velocity, channel;
+
+bool BluMode = true;
+//#######################################
+
+// Function Declarations #############################
+void printData();
+void BluetoothOut(int x, int y, int xStep, int yStep);
+void home();
+void moveCursor(int x, int y, int v);
+void leftClick();
+void printData();
+// #######################################
+
+void setup() {
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Wire.begin(1);
+  Serial.begin(57600);
+  delay(3500);
+  Serial.println("MIDI-TC pro Ready...");
+  if(BluMode == true){
+    Serial.println("Mode = Blutooth");
+  }
+  else if(BluMode == false){
+    Serial.println("Mode = USB");
+  }
+}
+
+// Main Program Loop ########################################
+void loop() {
+  if (MIDI.read()){
+    byte type = MIDI.getType();
+    //read MIDI data 
+    switch (type) {
+      case midi::NoteOn:
+        note = MIDI.getData1();
+        velocity = MIDI.getData2();
+        channel = MIDI.getChannel();
+        channel = channel-1;
+          //printData();  //Uncoment to see debug data
+        break;
+    }
+  }
+  //note = command for "full"
+  if(channel == Channel && note == Full_note && velocity >= Velocity_thresh ){            
+    Serial.println("Full");
+    if (BluMode == false){
+      moveCursor(15, 45, 3);  // move(x,y,v)
+    }
+    else if(BluMode == true){
+      BluetoothOut(5, 15, 15, 10);  // Bluetooth(x, y, xStep, yStep)
+    }
+    note = 0;
+  }
+  //note = command for "Worship"
+  else if(channel == Channel && note == Worship_note && velocity >= Velocity_thresh){            
+    Serial.println("Worship");
+    if (BluMode == false){
+      moveCursor(63, 47, 3); // move(x,y,v)
+    }
+    else if(BluMode == true){
+      BluetoothOut(21, 16, 15, 10);  // Bluetooth(x, y, xStep, yStep)
+    }
+    note = 0;
+  }
+}
+// END MAIN LOOP #############################################
+
+
+
+// FUNCTIONS ########################################################################################
+
+
+void printData(){
+  if (velocity > 0) {
+    Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+  } else {
+    Serial.println(String("Note Off: ch=") + channel + ", note=" + note);
+  }
+}
+
+void BluetoothOut(int x, int y, int xStep, int yStep){
+  Wire.beginTransmission(9);
+  Wire.print(x);
+  Wire.print("@");
+  Wire.print(y);
+  Wire.print("-");
+  Wire.print(xStep);
+  Wire.print("&");
+  Wire.print(yStep);
+  Wire.print("#");
+  Wire.endTransmission();
+}
+
+void home(){
+  int i;
+    for (i=0; i<50; i++) {
+     Mouse.move(-127, -127);
+   }
+}
+
+void moveCursor(int x, int y, int v){
+  home();
+  for (int i=0; i<x; i++){    // move Horizontal - X AXIS
+    Mouse.move(v, 0);
+  } 
+  for (int i=0; i<y; i++) {   // Move Verticl - Y AXIS
+    Mouse.move(0, v);
+  }
+  delay(25);
+  leftClick();
+  note = 0;
+}
+
+void leftClick(){
+  Mouse.press();
+  delay(100);
+  Mouse.release();
+  delay(250);
+}
